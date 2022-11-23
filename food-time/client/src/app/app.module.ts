de3454318798ee14b42266704732ba550cfda4ad
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-11-23 12:20:12 +0200

    Add fontawesome icons

--- food-time/client/src/app/app.module.ts
+++ food-time/client/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent],
 })
