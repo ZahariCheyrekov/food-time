f1d9b335ec7ec39397f16736ec5cb50aeeb17fc3
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-11-25 16:36:02 +0200

    Implement mongoose user schema validation

--- food-time/server/src/models/User.js
+++ food-time/server/src/models/User.js
@@ -2,24 +2,28 @@ import mongoose from 'mongoose';
 
 const userSchema = mongoose.Schema({
     fistName: {
-        type: String,
-        required: true
+        type: [String, 'First name should be of type string'],
+        minLength: [2, 'First name should be at least 2 characters long'],
+        required: [true, 'First name is required']
     },
     lastName: {
-        type: String,
-        required: true
+        type: [String, 'Last name should be of type string'],
+        minLength: [2, 'Last name should be at least 2 characters long'],
+        required: [true, 'Last name is required']
     },
     email: {
-        type: String,
-        required: true
+        type: [String, 'Email should be of type string'],
+        unique: [true, 'Email is already in use'],
+        minLength: [6, 'Email should be at least 6 characters long'],
+        required: [true, 'Email is required']
     },
     password: {
-        type: String,
-        required: true
+        type: [String, 'Password should be of type string'],
+        minLength: [6, 'Password must be at least 6 characters long'],
+        required: [true, 'Password is required']
     },
     picture: {
-        type: String,
-        required: true
+        type: [String, 'Picture should be of type string'],
     },
     createdDishes: [
         {
