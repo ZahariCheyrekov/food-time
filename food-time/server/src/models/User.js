198e54cbe19ebf64942c8d7d22c8986693afccbd
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-11-25 16:11:41 +0200

    Create user schema

--- food-time/server/src/models/User.js
+++ food-time/server/src/models/User.js
@@ -1,7 +1,44 @@
 import mongoose from 'mongoose';
 
 const userSchema = mongoose.Schema({
-    
+    fistName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    picture: {
+        type: String,
+        required: true
+    },
+    createdDishes: [
+        {
+            type: mongoose.Types.ObjectId,
+            ref: 'Dish'
+        }
+    ],
+    likedDishes: [
+        {
+            type: mongoose.Types.ObjectId,
+            ref: 'Dish'
+        }
+    ],
+    reviews: [
+        {
+            type: mongoose.Types.ObjectId,
+            ref: 'Review'
+        }
+    ]
 });
 
 const User = mongoose.model('User', userSchema);
