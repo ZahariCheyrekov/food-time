d8eeb965a3c79dac58e4f0c276a79203fbb3c0f5
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-11-26 22:49:13 +0200

    Apply dish schema validation

--- food-time/server/src/models/Dish.js
+++ food-time/server/src/models/Dish.js
@@ -1,25 +1,30 @@
-import mongoose, { mongo } from 'mongoose';
+import mongoose from 'mongoose';
 
 const dishSchema = mongoose.Schema({
     name: {
         type: [String, 'Name must be of type string'],
-        required: true
+        required: [true, 'Name is required']
     },
     ingredients: {
         type: [String, 'Ingredients must be of type string'],
-        required: true
+        required: [true, 'Ingredients are required']
     },
     price: {
         type: [Number, 'Price must be a number'],
-        required: true
+        validate: (price) => {
+            if (price <= 0) {
+                throw new Error('Price must be a positive number');
+            }
+        },
+        required: [true, 'Price is required']
     },
     description: {
         type: [String, 'Description must be of type string'],
-        required: true
+        required: [true, 'Description is required']
     },
     preparationTime: {
         type: [String, 'Preparation time must be of type string'],
-        required: true
+        required: [true, 'Preparation time is required']
     },
     ownerId: {
         type: mongoose.Types.ObjectId,
