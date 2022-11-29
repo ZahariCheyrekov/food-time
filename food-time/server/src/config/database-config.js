e3f250a82db3a5bc31ad8e3662e699d35c20dc33
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-11-29 20:40:11 +0200

    Fix database configuration

--- food-time/server/src/config/database-config.js
+++ food-time/server/src/config/database-config.js
@@ -4,11 +4,7 @@ import { app } from '../index.js';
 import { DEFAULT_PORT } from '../constants/app-constants.js';
 
 export const configDatabase = () => {
-    mongoose.connect(process.env.CONNECTION_URL,
-        {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        })
+    mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
         .then(() => app.listen(DEFAULT_PORT, () =>
             console.log(`Server is listening on: http://localhost:${DEFAULT_PORT}`)
         ))
