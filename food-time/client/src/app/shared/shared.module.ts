6dcf892c4d485d5d3ee48141e2b1d6e8db10851f
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-01 18:15:49 +0200

    Import footer component and fontawesome

--- food-time/client/src/app/shared/shared.module.ts
+++ food-time/client/src/app/shared/shared.module.ts
@@ -2,16 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { FooterComponent } from './components/footer/footer.component';
-
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  declarations: [
-    NavigationComponent,
-    FooterComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [NavigationComponent, FooterComponent],
+  imports: [CommonModule, FontAwesomeModule],
+  exports: [FooterComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
