ee1079293b82a0795189718e18c56248d45bb2c7
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-01 18:15:13 +0200

    Import shared modules

--- food-time/client/src/app/app.module.ts
+++ food-time/client/src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
+  imports: [BrowserModule, AppRoutingModule, SharedModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent],
 })
