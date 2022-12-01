48385db9ef26001d1bdaec39b7b32f44260f84ff
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-01 18:04:17 +0200

    Create footer component

--- food-time/client/src/app/shared/shared.module.ts
+++ food-time/client/src/app/shared/shared.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavigationComponent } from './components/navigation/navigation.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
-    NavigationComponent
+    NavigationComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule
