c9cefc0b0bd9bbba28f46ba44e1ddefff1a95e2a
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-02 18:58:08 +0200

    Fix auth module imports

--- food-time/client/src/app/auth/auth.module.ts
+++ food-time/client/src/app/auth/auth.module.ts
@@ -4,10 +4,10 @@ import { RouterModule } from '@angular/router';
 
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { AuthRoutingModule } from './auth-routing.module';
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent],
-  imports: [CommonModule, RouterModule],
-  exports: [LoginComponent, RegisterComponent],
+  imports: [CommonModule, RouterModule, AuthRoutingModule],
 })
 export class AuthModule {}
