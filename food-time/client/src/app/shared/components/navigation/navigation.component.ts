081b8bcc00095074a223a85315ebba9a3c1cd5c1
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-02 17:59:13 +0200

    Fix navigation component

--- food-time/client/src/app/shared/components/navigation/navigation.component.ts
+++ food-time/client/src/app/shared/components/navigation/navigation.component.ts
@@ -1,15 +1,8 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss'],
 })
-export class NavigationComponent {
-  constructor(private router: Router) {}
-
-  navigate(path: string): void {
-    this.router.navigate([path, 0]);
-  }
-}
+export class NavigationComponent {}
