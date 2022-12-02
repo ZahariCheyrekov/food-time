c96b07f8bea3e6da6431b14cb81200221a0231dc
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-02 18:58:31 +0200

    Configure navigation router links

--- food-time/client/src/app/shared/components/navigation/navigation.component.ts
+++ food-time/client/src/app/shared/components/navigation/navigation.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss'],
 })
-export class NavigationComponent {}
+export class NavigationComponent implements OnInit {
+  ngOnInit(): void {}
+}
