8e5bc9740a0aef21d2bb141ef51164ee6dc7659a
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-02 18:58:36 +0200

    Fix component

--- food-time/client/src/app/shared/shared.module.ts
+++ food-time/client/src/app/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [NavigationComponent, FooterComponent],
-  imports: [CommonModule, FontAwesomeModule],
+  imports: [CommonModule, FontAwesomeModule, RouterModule],
   exports: [NavigationComponent, FooterComponent],
 })
 export class SharedModule {}
