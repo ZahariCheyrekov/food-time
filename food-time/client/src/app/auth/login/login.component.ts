68d2a09ac654e7bbdffa8765605131b6381d7cd5
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-04 10:38:27 +0200

    Create loading state and on submit handler

--- food-time/client/src/app/auth/login/login.component.ts
+++ food-time/client/src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['../auth-form.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  isLoading = false;
+
   ngOnInit(): void {}
+
+  onSubmit(form: NgForm) {
+    const email = form.value.email;
+    const password = form.value.password;
+    this.isLoading = true;
+
+    console.log(form.value);
+   
+
+    form.reset();
+  }
 }
