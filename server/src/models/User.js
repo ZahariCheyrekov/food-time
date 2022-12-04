cd8c1a7e1b1009a2d070c97bbe5c7950e61a91c7
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-05 00:19:20 +0200

    Fix mongoose user schema

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,18 +1,13 @@
 import mongoose from 'mongoose';
 
 const userSchema = mongoose.Schema({
-    fistName: {
-        type: [String, 'First name should be of type string'],
-        minLength: [2, 'First name should be at least 2 characters long'],
-        required: [true, 'First name is required']
-    },
-    lastName: {
-        type: [String, 'Last name should be of type string'],
-        minLength: [2, 'Last name should be at least 2 characters long'],
-        required: [true, 'Last name is required']
+    name: {
+        type: String,
+        minLength: [5, 'Name should be at least 5 characters long'],
+        required: [true, 'Name is required']
     },
     email: {
-        type: [String, 'Email should be of type string'],
+        type: String,
         unique: [true, 'Email is already in use'],
         minLength: [6, 'Email should be at least 6 characters long'],
         validate: (email) => {
@@ -24,12 +19,12 @@ const userSchema = mongoose.Schema({
         required: [true, 'Email is required']
     },
     password: {
-        type: [String, 'Password should be of type string'],
+        type: String,
         minLength: [6, 'Password must be at least 6 characters long'],
         required: [true, 'Password is required']
     },
     picture: {
-        type: [String, 'Picture should be of type string'],
+        type: String,
     },
     createdDishes: [
         {
