f47c737c146d20e992c130116b2a0d73ad426adf
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-05 00:21:21 +0200

    Implement create user service function

--- server/src/services/auth-service.js
+++ server/src/services/auth-service.js
@@ -3,3 +3,7 @@ import User from '../models/User.js';
 export const getUserByEmail = (email) => {
     return User.findOne({ email });
 }
+
+export const createUser = (data) => {
+    return User.create(data);
+}
