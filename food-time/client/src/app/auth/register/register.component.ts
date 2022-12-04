148626560fa77bba70f531c86152b426c018a815
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-04 10:49:11 +0200

    Create register form on submit handler

--- food-time/client/src/app/auth/register/register.component.ts
+++ food-time/client/src/app/auth/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-register',
@@ -6,7 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['../auth-form.component.scss'],
 })
 export class RegisterComponent implements OnInit {
+  isLoading = false;
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  onSubmit(form: NgForm) {
+    const firstName = form.value.firstName;
+    const lastName = form.value.lastName;
+    const email = form.value.email;
+    const password = form.value.password;
+    const repeatPassword = form.value.repeatPassword;
+
+    console.log(form.value);
+
+    form.reset();
+  }
 }
