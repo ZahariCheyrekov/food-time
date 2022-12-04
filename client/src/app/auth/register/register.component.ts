40b7927744ca5cbcce128f7698a0978f2440fc01
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-05 00:19:41 +0200

    Add register component on submit logic

--- client/src/app/auth/register/register.component.ts
+++ client/src/app/auth/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+import { AuthService } from 'src/app/core/services/auth.service';
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -9,18 +11,21 @@ import { NgForm } from '@angular/forms';
 export class RegisterComponent implements OnInit {
   isLoading = false;
 
-  constructor() {}
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {}
 
   onSubmit(form: NgForm) {
-    const firstName = form.value.firstName;
-    const lastName = form.value.lastName;
+    const name = `${form.value.firstName} ${form.value.lastName}`;
     const email = form.value.email;
     const password = form.value.password;
     const repeatPassword = form.value.repeatPassword;
 
     console.log(form.value);
+
+    this.authService
+      .register(name, email, password, repeatPassword)
+      .subscribe((res) => console.log(res));
 
     form.reset();
   }
