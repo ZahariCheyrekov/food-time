b79323e35a0f5fe6adad5d992a86eaa189bbbbef
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-05 00:21:06 +0200

    Configure auth controller register user route

--- server/src/controllers/auth-controller.js
+++ server/src/controllers/auth-controller.js
@@ -3,6 +3,7 @@ import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
 import * as authService from '../services/auth-service.js';
+import { SALT, TOKEN_EXPIRATION_TIME } from '../constants/app-constants.js';
 
 
 const router = express.Router();
@@ -14,13 +15,13 @@ router.post('/login', async (req, res) => {
         const existingUser = await authService.getUserByEmail(email);
 
         if (!existingUser) {
-            return res.status(404).json({ message: 'User doesn\'t exist.' });
+            res.status(404).json({ message: 'User doesn\'t exist.' });
         }
 
         const isPasswordValid = await bcrypt.compare(password, existingUser.password);
 
         if (!isPasswordValid) {
-            return res.status(400).json({ message: 'Invalid credentials.' });
+            res.status(400).json({ message: 'Invalid credentials.' });
         }
 
         const token = jwt.sign({ email: existingUser.email, id: existingUser._id }, process.env.SECRET, { expiresIn: TOKEN_EXPIRATION_TIME });
@@ -28,6 +29,34 @@ router.post('/login', async (req, res) => {
         res.status(200).json({ result: existingUser, token });
 
     } catch (error) {
+        res.status(500).json({ message: 'Something went wrong.' });
+    }
+});
+
+router.post('/register', async (req, res) => {
+    const { name, email, password, repeatPassword } = req.body;
+    console.log(req.body)
+    try {
+        const existingUser = await authService.getUserByEmail(email);
+
+        if (existingUser) {
+            res.status(400).json({ message: 'User already exists.' });
+        }
+
+        if (password !== repeatPassword) {
+            res.status(400).json({ message: 'Passwords don\'t match.' });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, SALT);
+
+        const result = await authService.createUser({ name, email, password: hashedPassword });
+
+        const token = jwt.sign({ email: result.email, id: result._id }, process.env.SECRET, { expiresIn: TOKEN_EXPIRATION_TIME });
+
+        res.status(200).json({ result, token });
+
+    } catch (error) {
+        console.log(error);
         res.status(500).json({ message: 'Something went wrong.' });
     }
 });
