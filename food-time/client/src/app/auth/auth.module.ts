9b611901fc3848e9f5413e6d0930a40a1d1ecbc2
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-04 10:38:10 +0200

    Import angular forms module

--- food-time/client/src/app/auth/auth.module.ts
+++ food-time/client/src/app/auth/auth.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
@@ -8,6 +9,6 @@ import { AuthRoutingModule } from './auth-routing.module';
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent],
-  imports: [CommonModule, RouterModule, AuthRoutingModule],
+  imports: [CommonModule, RouterModule, AuthRoutingModule, FormsModule],
 })
 export class AuthModule {}
