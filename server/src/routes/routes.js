2910537691247565519915bf529cbcd2e120193a
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-04 23:50:30 +0200

    Import auth controller and use it

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -1,8 +1,10 @@
 import express from 'express';
 
+import authController from '../controllers/auth-controller.js';
+
 const router = express.Router();
 
-
+router.use('/auth', authController);
 
 export default router;
 
