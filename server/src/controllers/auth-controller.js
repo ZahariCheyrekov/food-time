a8cae7ce53c4318105cd82c8f6c2ce0dd4ed995e
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-05 11:19:53 +0200

    Fix auth controller routes data

--- server/src/controllers/auth-controller.js
+++ server/src/controllers/auth-controller.js
@@ -26,7 +26,13 @@ router.post('/login', async (req, res) => {
 
         const token = jwt.sign({ email: existingUser.email, id: existingUser._id }, process.env.SECRET, { expiresIn: TOKEN_EXPIRATION_TIME });
 
-        res.status(200).json({ result: existingUser, token });
+        const user = {
+            _id: existingUser._id,
+            name: existingUser.name,
+            email: existingUser.email,
+        }
+
+        res.status(200).json({ user, token });
 
     } catch (error) {
         res.status(500).json({ message: 'Something went wrong.' });
@@ -35,7 +41,7 @@ router.post('/login', async (req, res) => {
 
 router.post('/register', async (req, res) => {
     const { name, email, password, repeatPassword } = req.body;
-    console.log(req.body)
+
     try {
         const existingUser = await authService.getUserByEmail(email);
 
@@ -53,7 +59,13 @@ router.post('/register', async (req, res) => {
 
         const token = jwt.sign({ email: result.email, id: result._id }, process.env.SECRET, { expiresIn: TOKEN_EXPIRATION_TIME });
 
-        res.status(200).json({ result, token });
+        const user = {
+            _id: result._id,
+            name: result.name,
+            email: result.email,
+        }
+
+        res.status(200).json({ user, token });
 
     } catch (error) {
         console.log(error);
