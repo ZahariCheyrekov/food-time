42db06993f485285e42f71595403426ec1e931f4
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-05 11:09:44 +0200

    Fix city schema

--- server/src/models/City.js
+++ server/src/models/City.js
@@ -2,13 +2,13 @@ import mongoose from 'mongoose';
 
 const citySchema = mongoose.Schema({
     name: {
-        type: [String, 'Name must be of type string'],
+        type: String,
         minLength: [2, 'Name must be at least 2 characters long'],
         maxLength: [50, 'Name must be maximum 50 characters long'],
         required: [true, 'Name is required']
     },
     picture: {
-        type: [String, 'Picture must be of type string'],
+        type: String,
         validate: [/^https?:\/\//, 'Picture url must start with "http://" or "https://"'],
         required: [true, 'Picture is required']
     },
