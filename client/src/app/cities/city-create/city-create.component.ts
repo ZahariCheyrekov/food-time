2fdac70cf1728f7f4ad2ffa9047dc7bd2245210a
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-06 21:23:24 +0200

    Add on submit handler

--- client/src/app/cities/city-create/city-create.component.ts
+++ client/src/app/cities/city-create/city-create.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { UploadService } from './upload.service';
 
 @Component({
   selector: 'app-city-create',
   templateUrl: './city-create.component.html',
-  styleUrls: ['./city-create.component.scss']
+  styleUrls: ['./city-create.component.scss'],
 })
 export class CityCreateComponent {
+  files: File[] = [];
+  url: any;
 
+  constructor(private uploadService: UploadService) {}
+
+  onSelect(event: any) {
+    console.log(event);
+    this.files.push(...event.addedFiles);
+  }
+
+  onRemove(event: any) {
+    console.log(event);
+    this.files.splice(this.files.indexOf(event), 1);
+  }
+
+  async onSubmit(form: NgForm) {
+    if (!this.files[0]) {
+      return;
+    }
+
+    const fileData = this.files[0];
+    const data = new FormData();
+    data.append('file', fileData);
+    data.append('upload_preset', 'angular-cloudinary');
+    data.append('cloud_name', 'dhcdh9u9h');
+
+    this.uploadService.uploadImage(data).subscribe((response: any) => {
+      this.url = response.secure_url;
+    });
+  }
 }
