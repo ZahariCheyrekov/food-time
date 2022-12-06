ea4646aff0b165fe2798de4034876876d4e93504
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-06 22:19:42 +0200

    Import city controller and use it

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -1,10 +1,13 @@
 import express from 'express';
 
 import authController from '../controllers/auth-controller.js';
+import cityController from '../controllers/city-controller.js';
 
 const router = express.Router();
 
 router.use('/auth', authController);
+router.use('/cities', cityController);
+
 
 export default router;
 
