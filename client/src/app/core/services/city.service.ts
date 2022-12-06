d012397905ecb520eaff6715c815475f82aaca40
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-06 22:34:42 +0200

    Add create city service

--- client/src/app/core/services/city.service.ts
+++ client/src/app/core/services/city.service.ts
@@ -1,9 +1,13 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CityService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  createCity(data: Object) {
+    return this.http.post('http://localhost:5000/cities', data);
+  }
 }
