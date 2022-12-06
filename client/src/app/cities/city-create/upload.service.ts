46f30a40afddfd395597ec65ce4f4db4f96e9f17
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-06 21:23:12 +0200

    Implement upload service

---
+++ client/src/app/cities/city-create/upload.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UploadService {
+  constructor(private http: HttpClient) {}
+
+  uploadImage(data: FormData) {
+    return this.http.post<Object>(
+      `https://api.cloudinary.com/v1_1/dhcdh9u9h/image/upload`,
+      data
+    );
+  }
+}
