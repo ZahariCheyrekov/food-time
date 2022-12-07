6ded36a299290e76a0cb86356c29f3f76bec1fe7
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-07 22:27:57 +0200

    Import cities component

--- client/src/app/cities/cities.module.ts
+++ client/src/app/cities/cities.module.ts
@@ -7,9 +7,10 @@ import { RouterModule } from '@angular/router';
 import { AuthRoutingModule } from './cities-routing.module';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { CitiesComponent } from './cities/cities.component';
 
 @NgModule({
-  declarations: [CityCreateComponent],
+  declarations: [CityCreateComponent, CitiesComponent],
   imports: [
     CommonModule,
     RouterModule,
