77873fca19dac882f42c6dee8695576c5fc6e2e2
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-07 22:50:09 +0200

    Implement get cities controller functionality

--- server/src/controllers/city-controller.js
+++ server/src/controllers/city-controller.js
@@ -5,6 +5,17 @@ import * as cityService from '../services/city-service.js';
 const router = express.Router();
 
 
+router.get('/', async (req, res) => {
+    try {
+        const cities = await cityService.getCities();
+        res.status(200).json(cities);
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'Something went wrong.' });
+    }
+});
+
 router.post('/', async (req, res) => {
     const { city, country, picture } = req.body;
 
