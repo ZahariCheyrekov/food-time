272f6e565ef6a67ac056b439ad11e9a7e0a26e82
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-07 22:50:56 +0200

    Add cities array and implement ngOnInit logic

--- client/src/app/cities/cities/cities.component.ts
+++ client/src/app/cities/cities/cities.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { CityService } from '../../core/services/city.service';
 
 @Component({
   selector: 'app-cities',
   templateUrl: './cities.component.html',
-  styleUrls: ['./cities.component.scss']
+  styleUrls: ['./cities.component.scss'],
 })
-export class CitiesComponent {
+export class CitiesComponent implements OnInit {
+  cities: any = [];
 
+  constructor(private cityService: CityService) {}
+
+  async ngOnInit(): Promise<void> {
+    this.cityService.getCities().subscribe((res) => this.cities.push(res));
+  }
 }
