8a7c7b5e10cfa4039feec300e619f463c2437f3b
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-07 22:50:26 +0200

    Create get cities service

--- client/src/app/core/services/city.service.ts
+++ client/src/app/core/services/city.service.ts
@@ -7,6 +7,10 @@ import { Injectable } from '@angular/core';
 export class CityService {
   constructor(private http: HttpClient) {}
 
+  getCities() {
+    return this.http.get('http://localhost:5000/cities');
+  }
+
   createCity(data: Object) {
     return this.http.post('http://localhost:5000/cities', data);
   }
