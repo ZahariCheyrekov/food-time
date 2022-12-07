52aa25cc5dc2ae2ca5b63ff2d79111f920e24c4b
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-07 22:31:22 +0200

    Add cities routing

--- client/src/app/cities/cities-routing.module.ts
+++ client/src/app/cities/cities-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { CitiesComponent } from './cities/cities.component';
 import { CityCreateComponent } from './city-create/city-create.component';
 
 const routes: Routes = [
   {
     path: '',
     children: [
+      {
+        path: '',
+        component: CitiesComponent,
+      },
       {
         path: 'create',
         component: CityCreateComponent,
