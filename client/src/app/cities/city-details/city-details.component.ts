1c880e58e378a69ecdbefc09e98347c88d562a3a
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-08 13:13:10 +0200

    Add city id to the city details component

--- client/src/app/cities/city-details/city-details.component.ts
+++ client/src/app/cities/city-details/city-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-city-details',
@@ -6,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./city-details.component.scss'],
 })
 export class CityDetailsComponent implements OnInit {
+  cityId: string | null = '';
+
+  constructor(private activatedRoute: ActivatedRoute) {}
+
   ngOnInit() {
+    this.cityId = this.activatedRoute.snapshot.paramMap.get('id');
     this.getCityDishes();
   }
 
