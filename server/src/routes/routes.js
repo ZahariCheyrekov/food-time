2bd5d5e7aacbf5f262f0792cd558df4ce0afec34
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-09 13:18:01 +0200

    Fix routes

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -2,14 +2,14 @@ import express from 'express';
 
 import authController from '../controllers/auth-controller.js';
 import cityController from '../controllers/city-controller.js';
-import mealsController from '../controllers/meals-controller.js';
+import mealController from '../controllers/meal-controller.js';
 
 
 const router = express.Router();
 
 router.use('/auth', authController);
 router.use('/cities', cityController);
-router.use('/cities/:id/meals', mealsController);
+router.use('/cities/:id/meals', mealController);
 
 
 export default router;
