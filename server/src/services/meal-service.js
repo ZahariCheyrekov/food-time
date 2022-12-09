01a260f461bff6c14050f0b4cf0921c3a4e23162
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-09 13:44:13 +0200

    Add create meal service function

--- server/src/services/meal-service.js
+++ server/src/services/meal-service.js
@@ -1,6 +1,14 @@
 import Meal from '../models/Meal.js';
 
+import * as authService from './auth-service.js';
+
 
 export const getCityMeals = (mealsIds) => {
     return Meal.find({ _id: { $in: [('6391c2c98b202d4df7c869e9'), ('6391c2f78b202d4df7c869ec')] } })
+}
+
+export const createMeal = async (mealData, ownerId) => {
+    const meal = await Meal.create(mealData);
+    await authService.createUserMeal(ownerId, meal._id);
+    return meal;
 }
