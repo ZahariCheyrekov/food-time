91fd86a58a3503b1f53ec27241976f8a471d2031
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-09 21:17:13 +0200

    Fix create meal service

--- server/src/services/meal-service.js
+++ server/src/services/meal-service.js
@@ -1,14 +1,19 @@
 import Meal from '../models/Meal.js';
 
 import * as authService from './auth-service.js';
+import * as cityService from './city-service.js';
 
 
 export const getCityMeals = (mealsIds) => {
     return Meal.find({ _id: { $in: [('6391c2c98b202d4df7c869e9'), ('6391c2f78b202d4df7c869ec')] } })
 }
 
-export const createMeal = async (mealData, ownerId) => {
+export const createMeal = async (mealData, ownerId, cityId) => {
     const meal = await Meal.create(mealData);
-    await authService.createUserMeal(ownerId, meal._id);
+    const mealId = meal._id;
+
+    await authService.createUserMeal(ownerId, mealId);
+    await cityService.createCityMeal(cityId, mealId);
+
     return meal;
 }
