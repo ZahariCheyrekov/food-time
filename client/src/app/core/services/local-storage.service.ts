896a64a3f87edcd420be914bcdb7e443406cd1a6
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-09 11:56:44 +0200

    Implement save user local storage service

--- client/src/app/core/services/local-storage.service.ts
+++ client/src/app/core/services/local-storage.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LocalStorageService {
+  userItem = environment.app.user;
 
-  constructor() { }
+  constructor() {}
+
+  saveUser(user: Object | void) {
+    localStorage.setItem(this.userItem, JSON.stringify(user));
+  }
 }
