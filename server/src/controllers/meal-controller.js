b1a6027affbbb556377e5b344937bab0848a5150
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-09 13:42:51 +0200

    Implement create meal route controller

--- server/src/controllers/meal-controller.js
+++ server/src/controllers/meal-controller.js
@@ -4,6 +4,27 @@ import * as mealService from '../services/meal-service.js';
 
 const router = express.Router();
 
+router.post('/', async (req, res) => {
+    const {
+        name,
+        ingredients,
+        price,
+        description,
+        preparationTime,
+        picture,
+        ownerId }
+        = req.body;
 
+    try {
+        const meal = await mealService.createMeal({
+            name, ingredients, price, description, preparationTime, picture, ownerId
+        }, ownerId);
+        res.status(201).json(meal);
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'Something went wrong.' });
+    }
+})
 
 export default router;
