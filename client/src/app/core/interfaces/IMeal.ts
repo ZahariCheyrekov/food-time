e043e55e841812850ab2c2bdef99c16cdc0eedd2
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-10 12:16:15 +0200

    Implement meal interface

---
+++ client/src/app/core/interfaces/IMeal.ts
@@ -0,0 +1,12 @@
+export interface IMeal {
+  _id: string;
+  name: string;
+  ingredients: string;
+  price: number;
+  description: string;
+  preparationTime: string;
+  picture: string;
+  ownerId: string;
+  likes: Array<string>;
+  reviews: Array<string>;
+}
