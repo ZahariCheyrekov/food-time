ca70fb1fb1c4db1e72d464517cf46e5e0bee73b6
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-11 18:02:06 +0200

    Create edit meal component functionality

--- client/src/app/cities/city-meal-edit/city-meal-edit.component.ts
+++ client/src/app/cities/city-meal-edit/city-meal-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 
 import { IMeal } from 'src/app/core/interfaces/IMeal';
@@ -13,22 +14,41 @@ export class CityMealEditComponent implements OnInit {
   meal = {} as IMeal;
   mealId: string = '';
   cityId: string = '';
+  formGroup: FormGroup;
 
   constructor(
     private mealService: MealService,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private formBuilder: FormBuilder
   ) {}
 
   ngOnInit() {
     this.cityId = this.activatedRoute.snapshot.paramMap.get('id') as string;
     this.mealId = this.activatedRoute.snapshot.paramMap.get('mealId') as string;
 
+    this.initForm();
     this.fetchMeal();
   }
 
   fetchMeal() {
     this.mealService.getMeal(this.cityId, this.mealId).subscribe((res: any) => {
       this.meal = res;
+      this.initForm(res);
+    });
+  }
+
+  onEditMeal() {
+    this.mealService.editMeal(this.cityId, this.mealId, this.formGroup);
+  }
+
+  private initForm(meal?: IMeal) {
+    this.formGroup = this.formBuilder.group({
+      name: [meal?.name || ''],
+      ingredients: [meal?.ingredients || ''],
+      price: [meal?.price || ''],
+      description: [meal?.description || ''],
+      preparationTime: [meal?.preparationTime || ''],
+      picture: [meal?.picture || ''],
     });
   }
 }
