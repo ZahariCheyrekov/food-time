045364e1601228de6cfda8c20307866c374d06b1
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-11 12:20:13 +0200

    Add meal and city state

--- client/src/app/cities/city-meal-details/city-meal-details.component.ts
+++ client/src/app/cities/city-meal-details/city-meal-details.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { IMeal } from 'src/app/core/interfaces/IMeal';
 
 @Component({
   selector: 'app-city-meal-details',
   templateUrl: './city-meal-details.component.html',
-  styleUrls: ['./city-meal-details.component.scss']
+  styleUrls: ['./city-meal-details.component.scss'],
 })
-export class CityMealDetailsComponent {
+export class CityMealDetailsComponent implements OnInit {
+  meal = {} as IMeal;
+  mealId: string = '';
+  cityId: string = '';
 
+  constructor(private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.cityId = this.activatedRoute.snapshot.paramMap.get('id') as string;
+    this.mealId = this.activatedRoute.snapshot.paramMap.get('mealId') as string;
+
+    this.fetchMeal();
+  }
+
+  fetchMeal() {
+
+  }
 }
