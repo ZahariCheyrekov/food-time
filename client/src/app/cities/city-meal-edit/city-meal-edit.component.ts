1e99a43d2a73fccd04b2b15b4927d4ad55489a8b
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-11 17:06:11 +0200

    Implement edit meal component

--- client/src/app/cities/city-meal-edit/city-meal-edit.component.ts
+++ client/src/app/cities/city-meal-edit/city-meal-edit.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { IMeal } from 'src/app/core/interfaces/IMeal';
+import { MealService } from 'src/app/core/services/meal.service';
 
 @Component({
   selector: 'app-city-meal-edit',
   templateUrl: './city-meal-edit.component.html',
-  styleUrls: ['./city-meal-edit.component.scss']
+  styleUrls: ['./city-meal-edit.component.scss'],
 })
-export class CityMealEditComponent {
+export class CityMealEditComponent implements OnInit {
+  meal = {} as IMeal;
+  mealId: string = '';
+  cityId: string = '';
 
+  constructor(
+    private mealService: MealService,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.cityId = this.activatedRoute.snapshot.paramMap.get('id') as string;
+    this.mealId = this.activatedRoute.snapshot.paramMap.get('mealId') as string;
+
+    this.fetchMeal();
+  }
+
+  fetchMeal() {
+    this.mealService.getMeal(this.cityId, this.mealId).subscribe((res: any) => {
+      this.meal = res;
+    });
+  }
 }
