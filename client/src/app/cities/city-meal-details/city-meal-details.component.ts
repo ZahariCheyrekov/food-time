56c72d30595ea27d9a5b4b80087c7085f0068fcc
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-12 18:48:17 +0200

    Add like meal functionality

--- client/src/app/cities/city-meal-details/city-meal-details.component.ts
+++ client/src/app/cities/city-meal-details/city-meal-details.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 
 import { IMeal } from 'src/app/core/interfaces/IMeal';
 import { MealService } from 'src/app/core/services/meal.service';
+import { LocalStorageService } from 'src/app/core/services/local-storage.service';
 
 @Component({
   selector: 'app-city-meal-details',
@@ -13,18 +14,21 @@ export class CityMealDetailsComponent implements OnInit {
   meal = {} as IMeal;
   mealId: string = '';
   cityId: string = '';
+  userId: string = '';
   mealLikes = 0;
 
   constructor(
     private router: Router,
     private activatedRoute: ActivatedRoute,
-    private mealService: MealService
+    private mealService: MealService,
+    private localStorageService: LocalStorageService
   ) {}
 
   ngOnInit() {
     this.cityId = this.activatedRoute.snapshot.paramMap.get('id') as string;
     this.mealId = this.activatedRoute.snapshot.paramMap.get('mealId') as string;
 
+    this.userId = this.localStorageService.getUserId();
     this.fetchMeal();
   }
 
@@ -43,5 +47,11 @@ export class CityMealDetailsComponent implements OnInit {
     this.mealService.deleteMeal(this.mealId, this.cityId).subscribe(() => {
       this.router.navigate([`/cities/${this.cityId}/meals`]);
     });
+  }
+
+  onLikeMeal() {
+    this.mealService
+      .likeMeal(this.cityId, this.mealId, this.userId)
+      .subscribe(() => {});
   }
 }
