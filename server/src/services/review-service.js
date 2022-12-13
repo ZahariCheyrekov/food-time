5fcb3f816c5dc17e86dcd03aad1c322b866ba1a3
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-13 20:54:41 +0200

    Implement get meal reviews service

--- server/src/services/review-service.js
+++ server/src/services/review-service.js
@@ -3,6 +3,11 @@ import Review from '../models/Review.js';
 import * as authService from './auth-service.js';
 import * as mealService from './meal-service.js';
 
+
+export const getMealReviews = (mealId) => {
+    return Review.find({ mealId });
+}
+
 export const createReview = async (mealId, userId, name, description) => {
     const review = await Review.create({
         description, mealId,
