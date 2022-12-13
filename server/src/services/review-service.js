eefcd006e8765f137f6a1b38acf377c184ff78c7
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-13 13:11:40 +0200

    Implement create review service

--- server/src/services/review-service.js
+++ server/src/services/review-service.js
@@ -1,5 +1,23 @@
 import Review from '../models/Review.js';
 
+import * as authService from './auth-service.js';
+import * as mealService from './meal-service.js';
+
+export const createReview = async (mealId, userId, name, description) => {
+    const review = await Review.create({
+        description, mealId,
+        reviewOwner: {
+            name,
+            postedBy: userId
+        }
+    });
+    const reviewId = review._id;
+
+    await authService.createUserReview(userId, reviewId);
+    await mealService.createMealReview(mealId, reviewId);
+
+    return review;
+}
 
 export const removeReviews = (reviewIds) => {
     return Review.deleteMany({ _id: { $in: reviewIds } });
