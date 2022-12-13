e49a128edc3537323af24af9bf287e7ef6769829
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-13 13:11:22 +0200

    Import review controller and use it

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -3,6 +3,7 @@ import express from 'express';
 import authController from '../controllers/auth-controller.js';
 import cityController from '../controllers/city-controller.js';
 import mealController from '../controllers/meal-controller.js';
+import reviewController from '../controllers/review-controller.js';
 
 
 const router = express.Router();
@@ -10,5 +11,6 @@ const router = express.Router();
 router.use('/auth', authController);
 router.use('/cities', cityController);
 router.use('/cities', mealController);
+router.use('/cities', reviewController);
 
 export default router;
