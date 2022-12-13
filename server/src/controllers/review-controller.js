932090d5ca333bdcff6172086f95fa8deab00b0b
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-13 20:55:02 +0200

    Create get meal reviews controller route

--- server/src/controllers/review-controller.js
+++ server/src/controllers/review-controller.js
@@ -5,6 +5,19 @@ import * as reviewService from '../services/review-service.js';
 
 const router = express.Router();
 
+router.get('/:id/meals/:mealId/reviews', async (req, res) => {
+    const { mealId } = req.params;
+
+    try {
+        const reviews = await reviewService.getMealReviews(mealId);
+        res.status(200).json(reviews);
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'Something went wrong.' });
+    }
+});
+
 router.post('/:id/meals/:mealId/review', async (req, res) => {
     const { mealId } = req.params;
     const { userId, name, description } = req.body;
