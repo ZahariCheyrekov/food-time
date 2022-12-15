ef71263b298aea8563aae3142bb999260c352277
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-15 16:55:55 +0200

    Generate user service and implement get user

---
+++ client/src/app/core/services/user.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  url = environment.app.default_url;
+
+  constructor(private http: HttpClient) {}
+
+  getUser(userId: string) {
+    return this.http.get(`${this.url}/profile/${userId}`);
+  }
+}
