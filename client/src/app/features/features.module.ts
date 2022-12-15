570284cd5a91fced6e95af2faf1ca054944dbc32
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-15 16:24:05 +0200

    Import profile conponent

--- client/src/app/features/features.module.ts
+++ client/src/app/features/features.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { AboutComponent } from './pages/about/about.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
-  declarations: [AboutComponent, NotFoundComponent],
+  declarations: [AboutComponent, NotFoundComponent, ProfileComponent],
   imports: [CommonModule],
 })
 export class FeaturesModule {}
