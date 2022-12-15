12683c4f7be9bf540546c3cf5ebd6de58af556a6
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-15 17:44:45 +0200

    Import router module

--- client/src/app/features/features.module.ts
+++ client/src/app/features/features.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { AboutComponent } from './pages/about/about.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { ProfileComponent } from './profile/profile.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [AboutComponent, NotFoundComponent, ProfileComponent],
-  imports: [CommonModule],
+  declarations: [AboutComponent, NotFoundComponent],
+  imports: [CommonModule, RouterModule],
 })
 export class FeaturesModule {}
