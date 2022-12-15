1a8ef63a5cfc44ed5c99f9485a19830951234537
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-15 16:54:51 +0200

    Fetch profile user

--- client/src/app/features/profile/profile.component.ts
+++ client/src/app/features/profile/profile.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { IUserProfile } from 'src/app/core/interfaces/IUser';
+import { UserService } from 'src/app/core/services/user.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent {
+  userId = '';
+  user = {} as IUserProfile;
 
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private userService: UserService
+  ) {}
+
+  ngOnInit() {
+    this.userId = this.activatedRoute.snapshot.paramMap.get('userId') as string;
+    this.fetchUser();
+  }
+
+  fetchUser() {
+    this.userService.getUser(this.userId).subscribe((res: any) => {
+      this.user = res;
+      console.log(res);
+    });
+  }
 }
