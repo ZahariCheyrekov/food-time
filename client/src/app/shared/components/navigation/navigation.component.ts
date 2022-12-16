aec199d0fb4366ce208be5a578bbf0a0c80ef4c1
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-16 12:48:27 +0200

    Add cart navigation

--- client/src/app/shared/components/navigation/navigation.component.ts
+++ client/src/app/shared/components/navigation/navigation.component.ts
@@ -1,13 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { faCartPlus } from '@fortawesome/free-solid-svg-icons';
 
+import { LocalStorageService } from 'src/app/core/services/local-storage.service';
+
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss'],
 })
 export class NavigationComponent implements OnInit {
-  ngOnInit(): void {}
+  userId = '';
+
+  constructor(private localStorageService: LocalStorageService) {}
+
+  ngOnInit() {
+    this.userId = this.localStorageService.getUserId();
+  }
 
   faCartPlus = faCartPlus;
 }
