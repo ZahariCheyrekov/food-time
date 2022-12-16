c2720dfa44b81d18a44348a69fea5258b823f6c4
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-16 13:12:29 +0200

    Implement cart component logic

--- client/src/app/features/cart/cart.component.ts
+++ client/src/app/features/cart/cart.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { IMeal } from 'src/app/core/interfaces/IMeal';
+
+import { LocalStorageService } from 'src/app/core/services/local-storage.service';
+import { UserService } from 'src/app/core/services/user.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.scss']
+  styleUrls: ['./cart.component.scss'],
 })
 export class CartComponent {
+  userId = '';
+  meals: IMeal[] = [];
 
+  constructor(
+    private localStorageService: LocalStorageService,
+    private userService: UserService
+  ) {}
+
+  ngOnInit() {
+    this.userId = this.localStorageService.getUserId();
+
+    this.fetchUserMeals();
+  }
+
+  fetchUserMeals() {
+    this.userService.getCartMeals(this.userId).subscribe((res: any) => {
+      this.meals = res;
+    });
+  }
 }
