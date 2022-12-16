d1e3de196d5f07016eceed382b30ded2033f259a
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-16 21:47:51 +0200

    Create buy meal functionality

--- client/src/app/features/cart/cart.component.ts
+++ client/src/app/features/cart/cart.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-import { IMeal } from 'src/app/core/interfaces/IMeal';
 
+import { IMeal } from 'src/app/core/interfaces/IMeal';
 import { LocalStorageService } from 'src/app/core/services/local-storage.service';
 import { UserService } from 'src/app/core/services/user.service';
+import { MealService } from 'src/app/core/services/meal.service';
 
 @Component({
   selector: 'app-cart',
@@ -10,12 +11,16 @@ import { UserService } from 'src/app/core/services/user.service';
   styleUrls: ['./cart.component.scss'],
 })
 export class CartComponent {
+  isLoading = true;
   userId = '';
+  cart: any = [];
   meals: IMeal[] = [];
+  totalPrice = 0;
 
   constructor(
     private localStorageService: LocalStorageService,
-    private userService: UserService
+    private userService: UserService,
+    private mealService: MealService
   ) {}
 
   ngOnInit() {
@@ -26,7 +31,27 @@ export class CartComponent {
 
   fetchUserMeals() {
     this.userService.getCartMeals(this.userId).subscribe((res: any) => {
-      this.meals = res;
+      this.meals = res.cartMeals;
+      this.cart = res.cart;
+
+      this.calculateTotalPrice();
+
+      console.log(this.cart);
     });
+  }
+
+  calculateTotalPrice() {
+    let total = 0;
+
+    for (let i = 0; i < this.cart.length; i++) {
+      total += this.cart[i].quantity * this.meals[i].price;
+    }
+
+    this.totalPrice = Number(total.toFixed(2));
+    console.log(this.totalPrice, total);
+  }
+
+  onBuy(cityId: string, mealId: string) {
+    this.mealService.buyMeal(cityId, mealId, this.userId).subscribe(() => {});
   }
 }
