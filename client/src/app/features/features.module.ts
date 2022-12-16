c3d4a9679723710f3af25635a5569d4ab05b34c9
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-16 12:51:41 +0200

    Import cart component

--- client/src/app/features/features.module.ts
+++ client/src/app/features/features.module.ts
@@ -3,11 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { AboutComponent } from './pages/about/about.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
-import { ProfileComponent } from './profile/profile.component';
 import { RouterModule } from '@angular/router';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
-  declarations: [AboutComponent, NotFoundComponent],
+  declarations: [AboutComponent, NotFoundComponent, CartComponent],
   imports: [CommonModule, RouterModule],
 })
 export class FeaturesModule {}
