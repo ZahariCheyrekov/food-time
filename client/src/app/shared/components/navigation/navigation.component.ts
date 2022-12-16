c9345aa8445add90bd2c2717c52f6a6d8c11c9b5
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-16 12:46:17 +0200

    Add cart icon

--- client/src/app/shared/components/navigation/navigation.component.ts
+++ client/src/app/shared/components/navigation/navigation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faCartPlus } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navigation',
@@ -7,4 +8,6 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
   ngOnInit(): void {}
+
+  faCartPlus = faCartPlus;
 }
