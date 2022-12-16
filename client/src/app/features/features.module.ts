5ec979fd8ee658ef39bfbe27020346fb32d755eb
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-16 20:15:56 +0200

    Remove cart component

--- client/src/app/features/features.module.ts
+++ client/src/app/features/features.module.ts
@@ -4,10 +4,9 @@ import { CommonModule } from '@angular/common';
 import { AboutComponent } from './pages/about/about.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { RouterModule } from '@angular/router';
-import { CartComponent } from './cart/cart.component';
 
 @NgModule({
-  declarations: [AboutComponent, NotFoundComponent, CartComponent],
+  declarations: [AboutComponent, NotFoundComponent],
   imports: [CommonModule, RouterModule],
 })
 export class FeaturesModule {}
