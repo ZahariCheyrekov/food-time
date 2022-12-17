21ea3d1a6b2bf3fa0f1f0409064fbcdeee14823b
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-17 22:06:56 +0200

    Create login form state

--- client/src/app/auth/login/login.component.ts
+++ client/src/app/auth/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 import { AuthService } from 'src/app/core/services/auth.service';
@@ -12,14 +12,22 @@ import { AuthService } from 'src/app/core/services/auth.service';
 export class LoginComponent implements OnInit {
   isLoading = false;
 
+  loginForm: FormGroup = new FormGroup({
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    password: new FormControl(null, [Validators.required]),
+  });
+
   constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit() {}
 
-  onSubmit(form: NgForm) {
-    const email = form.value.email;
-    const password = form.value.password;
-    this.isLoading = true;
+  onSubmit() {
+    if (!this.loginForm.valid) {
+      return;
+    }
+
+    const email = this.loginForm.value.email;
+    const password = this.loginForm.value.password;
 
     this.authService.login(email, password).subscribe((res) => res);
 
