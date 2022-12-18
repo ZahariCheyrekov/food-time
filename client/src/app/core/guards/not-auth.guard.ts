3daf1a68b70c6b6a422510927a2b3f31ace5a988
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-18 12:06:33 +0200

    Generate not auth guard

---
+++ client/src/app/core/guards/not-auth.guard.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  CanLoad,
+  Route,
+  Router,
+  RouterStateSnapshot,
+  UrlSegment,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { LocalStorageService } from '../services/local-storage.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class NotAuthGuard implements CanActivate, CanLoad {
+  constructor(
+    private localStorageService: LocalStorageService,
+    private router: Router
+  ) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    return true;
+  }
+  canLoad(
+    route: Route,
+    segments: UrlSegment[]
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    if (!this.localStorageService.isAuthenticated()) {
+      return true;
+    }
+    this.router.navigate(['/']);
+    return false;
+  }
+}
