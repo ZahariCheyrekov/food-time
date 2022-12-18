1003fb9a942df679af1ab4a165087224b211fac5
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-18 12:06:13 +0200

    Apply routing guards

--- client/src/app/auth/auth-routing.module.ts
+++ client/src/app/auth/auth-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { NotAuthGuard } from '../core/guards/not-auth.guard';
 
 const routes: Routes = [
   {
@@ -11,10 +12,12 @@ const routes: Routes = [
       {
         path: 'login',
         component: LoginComponent,
+        canActivate: [NotAuthGuard],
       },
       {
         path: 'register',
         component: RegisterComponent,
+        canActivate: [NotAuthGuard],
       },
     ],
   },
